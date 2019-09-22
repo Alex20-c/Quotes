5b254a171ca2960b4553e7ca7aa3bd7c0c2640b0
Alex Kiprop <kipropalex59@gmail.com>
2019-09-22 12:58:02 +0300

    Add  funtionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuoteComponent } from './quote/quote.component';
-import { HighlightDirective } from './highlight.directive';
 import { DateCountPipe } from './date-count.pipe';
-import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { HighlightDirective } from './highlight.directive';
+
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.scss']
 })
 export class QuoteFormComponent implements OnInit {
-
+  newQuote = new Quote(0, "", "", "", new Date(), 0, 0);
+  @Output() addQuote = new EventEmitter<Quote>();
+  
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+  }
   constructor() { }
 
   ngOnInit() {

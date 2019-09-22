dee402d1be35db8c2f920482e8e1b49c8d7686a0
Alex Kiprop <kipropalex59@gmail.com>
2019-09-22 10:32:22 +0300

    import Quote from quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,12 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Quote } from '../quote';
+
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.scss']
 })
 export class QuoteComponent implements OnInit {
+  quite: Quote[] = [
+    new Quote(
+      1,
+      'If we acknowledge our need for God, he will help user',
+      'Ben Carson',
+      'Think big',
+      new Date(2018, 5, 16),
+      0,
+      0),
+      
+    new Quote(
+      2,
+      'Some are born great,some achieve greatness,and some have greatness thrust upon them',
+      'Twelfth Night',
+      'Shakespear',
+      new Date(1999, 1, 20),
+      0,
+      0),
 
+    new Quote(
+      3,
+      'Time wasted never come back',
+      'Albert',
+      'Thomson',
+      new Date(1998, 2, 16),
+      0,
+      0),
+  ];
+
+  
   constructor() { }
 
   ngOnInit() {

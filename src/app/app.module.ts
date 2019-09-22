184a91068a9d2e0b6495eaef25bbf4f6aba15e0f
Alex Kiprop <kipropalex59@gmail.com>
2019-09-22 09:37:55 +0300

    created files

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuoteComponent,
+    HighlightDirective,
+    DateCountPipe,
+    QuoteFormComponent,
+    QuoteDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
